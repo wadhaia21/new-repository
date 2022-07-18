0f1b93f799c800831130dedaf32a5a6708c7c8bb
Ashish <Ashish@DESKTOP-4DL6P7L>
2022-07-18 15:25:42 +0530

    "commit by ashish"

---
+++ src/sample_github/Exg_1.java
@@ -0,0 +1,18 @@
+package sample_github;
+
+public class Exg_1
+{
+    public static void main(String[] args)
+    {
+		System.out.println("Good Afternoon");
+		System.out.println("Good Afternoon");
+		System.out.println("Good Afternoon");
+		System.out.println("Good Afternoon");
+    	
+    	
+    	
+	}
+	
+	
+	
+}
